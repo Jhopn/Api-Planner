6b67e51fc1adae4cddacb13afa82da57efaeb9e9
Jhoão <jhpdrnsantos2@gmail.com>
2024-07-11 11:51:54 -0300

    feat: Interação com as rotas de participante

--- src/main/java/com/learning/planner/participant/ParticipantService.java
+++ src/main/java/com/learning/planner/participant/ParticipantService.java
@@ -1,5 +1,7 @@
 package com.learning.planner.participant;
 
+import com.learning.planner.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,7 +9,24 @@ import java.util.UUID;
 
 @Service
 public class ParticipantService {
-    public void registerParticipantsToEvents(List<String> participantsToInvite, UUID tripId){ }
-    public void triggerConfirmationEmailToParticipants(){}
+    @Autowired
+    private ParticipantRepository repository;
+    public void registerParticipantsToEvents(List<String> participantsToInvite, Trip trip){
+        List<Participant> participants = participantsToInvite.stream().map(email -> new Participant(email, trip)).toList();
+        this.repository.saveAll(participants);
+    }
+    public ParticipantCreateResponse registerParticipantToEvent(String email, Trip trip){
+        Participant newParticipant = new Participant(email, trip);
+        this.repository.save(newParticipant);
+
+        return new ParticipantCreateResponse(newParticipant.getId());
+    }
+
+    public List<ParticipantData> getAllParticipantFromEvent(UUID tripId){
+        return this.repository.findByTripId(tripId).stream().map(participant -> new ParticipantData(participant.getId(), participant.getName(), participant.getEmail(), participant.getIsConfirmed())).toList();
+    }
+    public void triggerConfirmationEmailToParticipants(UUID id){}
+
+    public void triggerConfirmationEmailToParticipant(String email){}
 }
 
